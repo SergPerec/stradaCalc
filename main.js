45e41d5cd9e8301fd542f69f26250b70777585ec
SergPerec <prahatuc@mail.ru>
2022-10-18 21:33:57 +0400

    add object in stradaCalc

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
-// calc('add', 1, 2) - возвращает 3
-// calc('multi', 1, 2) - возвращает 2
-// calc('subtract', 3, 2) - возвращает 1
+const operations = {
+  add: '+',
+  multi: '*',
+  substract: '%',
+};
 
 function calc(oper, oneNum, secNum) {
+  if ( isNaN(oneNum) || isNaN(secNum) ) {
+    return 'Вы ввели не число.';
+  } else if (oper === operations['add']) {
+    return oneNum + secNum;
+  } else if (oper === operations['multi']) {
+    return oneNum * secNum;
+  } else if (oper === operations['substract']) {
+    return oneNum % secNum;
+  } 
 
-  switch(oper) {
-    case 'add':
-      return oneNum + secNum;
-      break;
-      case 'multi':
-      return oneNum * secNum;
-      break;
-      case 'subtract':
-      return oneNum % secNum;
-      break;
-  }
-
-  // if (oper === 'add') {
-  //   return oneNum + secNum;
-  // } else if (oper === 'multi') {
-  //   return oneNum * secNum;
-  // } else if (oper === 'subtract') {
-  //   return oneNum % secNum;
-  // }
 }
 
-console.log(calc('add', 1, 2))
+console.log(calc('+', 5, 7));
