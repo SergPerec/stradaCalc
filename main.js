9fb33bb3a46e06baca2df3d78acfcca3dd8f863f
SergPerec <prahatuc@mail.ru>
2022-11-09 23:05:57 +0400

    add UI

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-const operations = {
-  add: '+',
-  multi: '*',
-  substract: '%',
+const OPERATIONS = {
+  PLUS: '+',
+  MINUS: '-',
+  MULTI: '*',
+  DIVISION: '/',
 };
-
-function calc(oper, oneNum, secNum) {
-  if ( isNaN(oneNum) || isNaN(secNum) ) {
-    return 'Вы ввели не число.';
-  } else if (oper === operations['add']) {
-    return oneNum + secNum;
-  } else if (oper === operations['multi']) {
-    return oneNum * secNum;
-  } else if (oper === operations['substract']) {
-    return oneNum % secNum;
-  } 
-
+function calc() {
+  const oneNum = +document.getElementById("inputOne").value;
+  const secNum = +document.getElementById("inputSecond").value;
+  const oper = document.getElementById("select").value;
+  let res
+  if (oper === OPERATIONS.PLUS) {
+    res = oneNum + secNum;
+  } else if (oper === OPERATIONS.MINUS) {
+    res = oneNum - secNum;
+  } else if (oper === OPERATIONS.MULTI) {
+    res = oneNum * secNum;
+  } else if (oper === OPERATIONS.DIVISION) {
+    res = oneNum / secNum;
+  }
+  document.getElementById('res').innerHTML = res.toFixed(3);
 }
-
-console.log(calc('+', 5, 7));
